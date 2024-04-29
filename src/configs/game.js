86d6f85de67f629bc8851070afe21ba21bf38ccf
delafuentej <jfuentelucas@gmail.com>
2024-04-29 10:12:04 +0200

    settings/react icons added

---
+++ src/configs/game.js
@@ -0,0 +1,14 @@
+
+//import { BsHandThumbsUp, BsHandThumbsDown } from "react-icons/bs";
+
+
+export const settings = {
+    gameName: "Rock, paper, scissors!",
+    userName: "Player 1",
+    pcName: "Computer",
+    winMessage: "You are the winner! Congratulations!",
+    tieMessage: "It is a tie",
+    lostMessage: "You are the loser! Try again!",
+    waitingMessage: "Select an option!",
+    winTarget:8,
+};
